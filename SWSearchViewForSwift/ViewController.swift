ebc59cf24a3b05b56986131603b73b744b1cba6a
anseee <admin@ansee.kr>
2016-07-16 10:08:42 +0900

    Initial Commit

---
+++ SWSearchViewForSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SWSearchViewForSwift
+//
+//  Created by 박성원 on 2016. 7. 16..
+//  Copyright © 2016년 ParkSungWon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
