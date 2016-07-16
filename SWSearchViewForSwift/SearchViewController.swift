653ea82aac6adf143d105dbd4c233e63853795c0
anseee <admin@ansee.kr>
2016-07-16 11:29:41 +0900

    Main.storyboard, SearchViewController.swift add SearchViewController in Container View and make views

---
+++ SWSearchViewForSwift/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  SWSearchViewForSwift
+//
+//  Created by 박성원 on 2016. 7. 16..
+//  Copyright © 2016년 ParkSungWon. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
