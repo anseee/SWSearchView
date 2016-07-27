51cfe7286242ae34bfe32ca6b142c897a86a3d45
anseee <admin@ansee.kr>
2016-07-27 23:12:55 +0900

    Main.storyboard, SearchViewController.swift In search view, if touch the background shadow view, can dismiss search view

--- SWSearchViewForSwift/SearchViewController.swift
+++ SWSearchViewForSwift/SearchViewController.swift
@@ -13,31 +13,22 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var dismissButton: UIButton!
+    
+    let NOTI_NAME = "hideSearchContainerView"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         self.searchBar.delegate = self
+        self.dismissButton .addTarget(self, action: #selector(SearchViewController.dismissView), forControlEvents: .TouchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func dismissView() {
+        NSNotificationCenter.defaultCenter().postNotificationName(NOTI_NAME, object: nil)
     }
     
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-        NSNotificationCenter.defaultCenter().postNotificationName("hideSearchContainerView", object: nil)
+        NSNotificationCenter.defaultCenter().postNotificationName(NOTI_NAME, object: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
